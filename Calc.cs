f2a6d4f9e06de91d6570e200e49778e1e3b4878f
abc <abc@gmail.com>
2024-02-16 10:11:48 +0530

    first commit

---
+++ Calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalApp
+{
+    public class Calc
+    {
+        public void Add(double x, double y)
+        {
+            Console.WriteLine("Result after addition:\t" +(x+y));
+        }
+        public void Sub(double x, double y)
+        {
+            Console.WriteLine("Result after Substraction:\t" + (x - y));
+        }
+        public void Multi(double x, double y)
+        {
+            Console.WriteLine("Result after Multiplication:\t" + (x * y));
+        }
+        public void Div(double x, double y)
+        {
+            Console.WriteLine("Result after Division:\t" + (x / y));
+        }
+    }
+}
